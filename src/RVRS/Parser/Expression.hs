b8ba5a286ba722179f9190d76061b4512fce3b27
CJ <rivera.carlos91@gmail.com>
2025-05-11 06:25:54 +0000

    Refactor: modularized parser

---
+++ src/RVRS/Parser/Expression.hs
@@ -0,0 +1,81 @@
+-- src/RVRS/Parser/Expression.hs
+
+module RVRS.Parser.Expression (exprParser) where
+
+import RVRS.AST
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
+import Control.Monad (void)
+import Control.Monad.Combinators.Expr
+import Data.Void
+
+type Parser = Parsec Void String
+
+-- Expression entry point
+exprParser :: Parser Expr
+exprParser = makeExprParser term operatorTable
+
+-- Operator precedence table
+operatorTable :: [[Operator Parser Expr]]
+operatorTable =
+  [ [ InfixL (Mul <$ symbol "*")
+    , InfixL (Div <$ symbol "/")
+    ]
+  , [ InfixL (Add <$ symbol "+")
+    , InfixL (Sub <$ symbol "-")
+    ]
+  , [ InfixL (Equals <$ symbol "==") ]
+  , [ InfixL (And <$ symbol "and")
+    , InfixL (Or <$ symbol "or")
+    ]
+  ]
+
+-- Terms in the expression grammar
+term :: Parser Expr
+term =
+      try callExprParser
+  <|> try (BoolLit True <$ symbol "truth")
+  <|> try (BoolLit False <$ symbol "void")
+  <|> try (StrLit <$> stringLiteral)
+  <|> try parseNumber
+  <|> try (Not <$> (symbol "not" *> term))
+  <|> try (Neg <$> (symbol "-" *> term))
+  <|> try (parens exprParser)
+  <|> Var <$> identifier
+
+-- Parse numeric literals
+parseNumber :: Parser Expr
+parseNumber = do
+  num <- lexeme $ try L.float <|> (fromInteger <$> L.decimal)
+  return $ NumLit num
+
+-- Function call expressions
+callExprParser :: Parser Expr
+callExprParser = do
+  _ <- symbol "call"
+  name <- identifier
+  args <- between (symbol "(") (symbol ")") (exprParser `sepBy` symbol ",")
+  return $ CallExpr name args
+
+-- Utility parsers
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme sc
+
+symbol :: String -> Parser String
+symbol = L.symbol sc
+
+parens :: Parser a -> Parser a
+parens = between (symbol "(") (symbol ")")
+
+identifier :: Parser String
+identifier = lexeme $ (:) <$> letterChar <*> many (alphaNumChar <|> char '_')
+
+stringLiteral :: Parser String
+stringLiteral = char '"' *> manyTill L.charLiteral (char '"')
+
+sc :: Parser ()
+sc = L.space space1 lineCmnt blockCmnt
+  where
+    lineCmnt  = L.skipLineComment "--"
+    blockCmnt = L.skipBlockComment "{-" "-}"
---
+++ src/RVRS/Parser/Statement.hs
@@ -0,0 +1,133 @@
+-- src/RVRS/Parser/Statement.hs
+
+module RVRS.Parser.Statement (statementParser, blockParser) where
+
+import RVRS.AST
+import RVRS.Parser.Expression (exprParser)
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
+import Control.Monad (void)
+import Control.Monad.Combinators.Expr
+import Data.Void
+
+-- Local parser type alias
+
+import Data.Char (isAlphaNum)
+
+type Parser = Parsec Void String
+
+-- Top-level statement parser
+statementParser :: Parser Statement
+statementParser = lexeme $
+      try pillarParser
+  <|> try mouthParser
+  <|> try whisperParser  
+  <|> try assertParser   
+  <|> try echoParser
+  <|> try sourceParser
+  <|> try deltaParser
+  <|> try branchParser
+  <|> try returnParser
+  <|> try callStmt
+
+-- Individual statement parsers
+whisperParser :: Parser Statement
+whisperParser = do
+  _ <- symbol "whisper"
+  expr <- exprParser
+  return $ Whisper expr
+
+assertParser :: Parser Statement
+assertParser = do
+  _ <- symbol "assert"
+  expr <- exprParser
+  return $ Assert expr
+
+mouthParser :: Parser Statement
+mouthParser = do
+  _ <- symbol "mouth"
+  expr <- exprParser  
+  return $ Mouth expr
+
+echoParser :: Parser Statement
+echoParser = do
+  _ <- symbol "echo"
+  expr <- exprParser  
+  return $ Echo expr
+
+sourceParser :: Parser Statement
+sourceParser = do
+  _ <- symbol "source"
+  var <- identifier
+  _ <- symbol "="
+  expr <- exprParser
+  return $ Source var expr
+
+deltaParser :: Parser Statement
+deltaParser = do
+  _ <- symbol "delta"
+  var <- identifier
+  _ <- optional (symbol "=")
+  expr <- exprParser
+  return $ Delta var expr
+
+pillarParser :: Parser Statement
+pillarParser = do
+  _ <- symbol "pillar"
+  var <- identifier
+  _ <- symbol "="
+  expr <- exprParser
+  return $ Pillar var expr
+
+returnParser :: Parser Statement
+returnParser = do
+  _ <- symbol "return"
+  expr <- exprParser
+  return $ Return expr
+
+branchParser :: Parser Statement
+branchParser = do
+  _ <- symbol "branch"
+  cond <- exprParser
+  thenBlock <- blockParser
+  elseBlock <- optionalElseParser
+  return $ Branch cond thenBlock elseBlock
+
+optionalElseParser :: Parser [Statement]
+optionalElseParser =
+      (symbol "else" *> blockParser)
+  <|> pure []
+
+blockParser :: Parser [Statement]
+blockParser = do
+  _ <- symbol "{"
+  stmts <- many statementParser
+  _ <- symbol "}"
+  return stmts
+
+callStmt :: Parser Statement
+callStmt = do
+  _ <- symbol "call"
+  name <- identifier
+  args <- option [] (parens (exprParser `sepBy` symbol ","))
+  return $ Call name args
+
+-- Shared utility parsers
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme sc
+
+symbol :: String -> Parser String
+symbol = L.symbol sc
+
+parens :: Parser a -> Parser a
+parens = between (symbol "(") (symbol ")")
+
+identifier :: Parser String
+identifier = lexeme $ (:) <$> letterChar <*> many (alphaNumChar <|> char '_')
+
+sc :: Parser ()
+sc = L.space space1 lineCmnt blockCmnt
+  where
+    lineCmnt  = L.skipLineComment "--"
+    blockCmnt = L.skipBlockComment "{-" "-}"
