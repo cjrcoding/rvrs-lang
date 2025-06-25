4b0400537423d6dff35415776aed8d1327246c76
Murat Kasimov <iokasimov.m@gmail.com>
2025-06-25 13:32:05 +0700

    Start prototyping implementation of `Calling` expression

--- src/RVRS/Engine.hs
+++ src/RVRS/Engine.hs
@@ -44,43 +44,49 @@ expression :: Recursive Expression `AR__` Engine `T'I` Value
 expression x = case unwrap x of
  Literal val -> intro @Engine `hv` val
  Variable var -> intro @Engine `hv` Unit
-   `yuk____` Old `hv__` State `ha` Event `hv` get @Bindings `yo` find var
-   `yok____` Try `ha__` None `hu_` Error `ha` Runtime `hv` Unbound var `la` Ok
+  `yuk____` Old `hv__` State `ha` Event `hv` get @Bindings `yo` find var
+  `yok____` Try `ha__` None `hu_` Error `ha` Runtime `hv` Unbound var `la` Ok
  Operator (Binary (These (These x y) (Comparison operation))) -> intro @Engine `hv` Unit
-   `yuk____` Run `hv` expression x
-      `lu'yp` Run `hv` expression y
-   `yok____` Try `ha` tap `ha` on @Double `ha` this
-      `lo'yp` Try `ha` tap `ha` on @Double `ha` that
-   `ho___'yo` Greater `hu` (is `ho'hd` (>) `ho` Bool)
-        `la` Equals `hu` (is `ho'hd` (==) `ho` Bool)
-        `la` Less `hu` (is `ho'hd` (<) `ho` Bool)
-        `li` is @Comparison operation
+  `yuk____` Run `hv` expression x
+     `lu'yp` Run `hv` expression y
+  `yok____` Try `ha` tap `ha` on @Double `ha` this
+     `lo'yp` Try `ha` tap `ha` on @Double `ha` that
+  `ho___'yo` Greater `hu` (is `ho'hd` (>) `ho` Bool)
+       `la` Equals `hu` (is `ho'hd` (==) `ho` Bool)
+       `la` Less `hu` (is `ho'hd` (<) `ho` Bool)
+       `li` is @Comparison operation
  Operator (Binary (These (These x y) (Arithmetic operation))) -> intro @Engine `hv` Unit
-   `yuk____` Run `hv` expression x
-      `lu'yp` Run `hv` expression y
-   `yok____` Try `ha` tap `ha` on @Double `ha` this
-      `lo'yp` Try `ha` tap `ha` on @Double `ha` that
-   `ho___'yo` Add `hu` (is `ho'hd` (+) `ho` Double)
-        `la` Sub `hu` (is `ho'hd` (-) `ho` Double)
-        `la` Mul `hu` (is `ho'hd` (*) `ho` Double)
-        `la` Div `hu` (is `ho'hd` (/) `ho` Double)
-        `li` is @Arithmetic operation
+  `yuk____` Run `hv` expression x
+     `lu'yp` Run `hv` expression y
+  `yok____` Try `ha` tap `ha` on @Double `ha` this
+     `lo'yp` Try `ha` tap `ha` on @Double `ha` that
+  `ho___'yo` Add `hu` (is `ho'hd` (+) `ho` Double)
+       `la` Sub `hu` (is `ho'hd` (-) `ho` Double)
+       `la` Mul `hu` (is `ho'hd` (*) `ho` Double)
+       `la` Div `hu` (is `ho'hd` (/) `ho` Double)
+       `li` is @Arithmetic operation
  Operator (Binary (These (These x y) (Combinated operation))) -> intro @Engine `hv` Unit
-   `yuk____` Run `hv` expression x
-      `lu'yp` Run `hv` expression y
-   `yok____` Try `ha` tap `ha` on @Bool `ha` this
-      `lo'yp` Try `ha` tap `ha` on @Bool `ha` that
-   `ho___'yo` And `hu` (is `ho'hd` (&&) `ho` Bool)
-        `la` Or `hu` (is `ho'hd` (||) `ho` Bool)
-        `li` is @Combinated operation
+  `yuk____` Run `hv` expression x
+     `lu'yp` Run `hv` expression y
+  `yok____` Try `ha` tap `ha` on @Bool `ha` this
+     `lo'yp` Try `ha` tap `ha` on @Bool `ha` that
+  `ho___'yo` And `hu` (is `ho'hd` (&&) `ho` Bool)
+       `la` Or `hu` (is `ho'hd` (||) `ho` Bool)
+       `li` is @Combinated operation
+ -- Calling name args -> intro @Engine `hv` Unit
+  -- `yuk____` Run `hv__` Given `hv` is @Flowings `yo` find name
+  -- `yok____` Try `ha__` None `hu_` Error `ha` Runtime `hv` Unknown name `la` Ok
+
+      -- Just (FlowIR _ paramNames body) -> do
+        -- argVals <- for (toList args) evalExpr
+        -- if length paramNames /= length argVals
+          -- then throwError $ RuntimeError ("Arity mismatch calling: " ++ name)
+          -- else fromJust `ha` fst <$> do callBody (toList body) `ha` fromList $ zip paramNames argVals
 
 tap :: forall target . Value `M` target `S` target `AR___` Error Reason target
 tap = Some `hu_` Error `ha` Runtime `ha` Require `hv` Unit `la` Valid @target
 
 -- evalBody stmts = stmts `yokl` Forth `ha` Run `ha` evaluate
 
--- evaluate x = statement x `yok_` Try `ha__` Continue `la` Interrupt `ha` Returns
-
--- evalBody :: [Recursive Statement] -> EvalIR (Maybe Value)
--- evalBody [] = return Nothing
--- evalBody (stmt:rest) = evalStmt stmt >>= maybe (evalBody rest) (pure `ha` Just)
+evaluate :: Recursive Statement `AR__` Engine Unit
+evaluate x = statement x `yok_` Try `ha__` Continue `la` Interrupt `ha` Returns
