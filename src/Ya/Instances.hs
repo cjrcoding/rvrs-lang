9d67ecad44521e1fa5f4b57a6325b88be5cd2ed3
Murat Kasimov <iokasimov.m@gmail.com>
2025-08-07 22:02:08 +0700

    Define some `Ord` instances for Ya primitives

--- src/Ya/Instances.hs
+++ src/Ya/Instances.hs
@@ -14,34 +14,36 @@ import Data.Map (Map)
 
 -- Deriving instances for Recursive, P, and S
 deriving instance (Eq (f (Recursive f))) => Eq (Recursive f)
-deriving instance (Show (f (Recursive f))) => Show (Recursive f)
-
 deriving instance (Eq l, Eq r) => Eq (l `P` r)
 deriving instance (Eq l, Eq r) => Eq (l `S` r)
-
 deriving instance (Eq l, Eq r) => Eq (T'I'II P l r)
 deriving instance (Eq l, Eq r) => Eq (T'I'II S l r)
-
 deriving instance (Eq l, Eq r) => Eq (T'II'I P r l)
 deriving instance (Eq l, Eq r) => Eq (T'II'I S r l)
-
 deriving instance (Eq (tt i (t ii)), Eq i, Eq ii) => Eq (TT'I'T'II t tt i ii)
 deriving instance (Eq i) => Eq (Construction Optional i)
-
 deriving instance (Eq i) => Eq (Tagged tag i)
 
+deriving instance (Ord (f (Recursive f))) => Ord (Recursive f)
+deriving instance (Ord l, Ord r) => Ord (l `P` r)
+deriving instance (Ord l, Ord r) => Ord (l `S` r)
+deriving instance (Ord l, Ord r) => Ord (T'I'II P l r)
+deriving instance (Ord l, Ord r) => Ord (T'I'II S l r)
+deriving instance (Ord l, Ord r) => Ord (T'II'I P r l)
+deriving instance (Ord l, Ord r) => Ord (T'II'I S r l)
+deriving instance (Ord (tt i (t ii)), Ord i, Ord ii) => Ord (TT'I'T'II t tt i ii)
+deriving instance (Ord i) => Ord (Construction Optional i)
+deriving instance (Ord i) => Ord (Tagged tag i)
+
+deriving instance (Show (f (Recursive f))) => Show (Recursive f)
 deriving instance (Show l, Show r) => Show (l `P` r)
 deriving instance (Show l, Show r) => Show (l `S` r)
-
 deriving instance (Show l, Show r) => Show (T'I'II P l r)
 deriving instance (Show l, Show r) => Show (T'I'II S l r)
-
 deriving instance (Show l, Show r) => Show (T'II'I P r l)
 deriving instance (Show l, Show r) => Show (T'II'I S r l)
-
 deriving instance (Show (tt i (t ii)), Show i, Show ii) => Show (TT'I'T'II t tt i ii)
 deriving instance (Show i) => Show (Construction Optional i)
-
 deriving instance (Show i) => Show (Tagged tag i)
 
 instance Setoid AR String where equality (These x y) = if x == y then That y else This (These x y)
