391ce6cd697cae01bdae0a57ffdd7aec5e7b08e3
Murat Kasimov <iokasimov.m@gmail.com>
2025-08-12 18:51:56 +0700

    Complete declaring typechecking of `Calling` expression

--- src/RVRS/Checker.hs
+++ src/RVRS/Checker.hs
@@ -19,14 +19,14 @@ type Flows = Map Name `T` List Typed
 
 type Context = Terms `P` Flows
 
-type Types = (Typed `P` Typed) `S` (Typed `P` Typed) `S` String `S` Name
+type Mistyped = (Typed `P` Typed) `S` (Typed `P` Typed) `S` (List Typed `P` List Typed) `S` Name
 
-pattern Mismatched x = This (This (This x)) :: Types
-pattern Unexpected x = This (This (That x)) :: Types
-pattern Unsupported x = This (That x) :: Types
-pattern Unknown x = That x :: Types
+pattern Mismatched x = This (This (This x)) :: Mistyped
+pattern Unexpected x = This (This (That x)) :: Mistyped
+pattern Misaligned x = This (That x) :: Mistyped
+pattern Unknown x = That x :: Mistyped
 
-type Checker = Stops Types `JNT` State Context
+type Checker = Stops Mistyped `JNT` State Context
 
 expression = is @(Recursive Expression `AR_` Checker Typed)
  `li__` literal `ha'he` is `la` variable `ha'he` is
@@ -35,21 +35,19 @@ expression = is @(Recursive Expression `AR_` Checker Typed)
 
 literal = intro @Checker `ha__` be `hv'he` String `la` be `hv'he` Double `la` be `hv'he` Bool
 
-variable x = intro @Checker `hv` Unit
- `yuk____` Lease `ha__` State `hv___` Event `hv` get `ha__` Scope `hv` at @Terms `ho_` Scope `hv` key x
- `yok____` Check `ha__` Break @Types `ha` Unknown `la` Ok @Typed
+variable var = intro @Checker `hv` Unit
+ `yuk____` Lease `ha__` State `hv___` Event `hv` get `ha__` Scope `hv` at @Terms `ho_` Scope `hv` key var
+ `yok____` Check `ha__` Error @Mistyped `ha` Unknown `la` Ok @Typed
 
-calling :: Instead Name (Recursive Expression) `P` List (Recursive Expression) `AR__` Checker Typed
 calling (These name args) = intro @Checker `hv` Unit
  `yuk______` Lease `ha__` State `hv___` Event `hv` get `ha__` Scope `hv` at @Flows `ho_` Scope `ha` key `hv'he` name
- `yok______` Apply `ha__` (\_ -> args `yokl` Forth `ha` Apply `ha` expression)
- `lo__'yp'yo'q` Check `ha__` Break @Types `ha` Unknown `la` Ok @(List Typed)
- -- `yok______` Check `ha__` Error `ha` Mismatched `la` Ok
- -- TODO: Stops Name (List Typed) -> Checker `L` Checker `T` Void `T` (List Typed `P` List Typed `S` List Typed))
- `ho___'yu` Bool Unit
+ `yok______` Apply `ha__` Exist `hu` args `ho_'yokl` Forth `ha` Apply `ha` expression
+ `lo__'yp'yo'q` Check `ha__` Error `ha` Unknown `la` Ok @(List Typed)
+ `yok______` Check `ha__` Error `ha` Misaligned `la` Ok @(List Typed)
+ `ho_____'yu` Bool Unit
 
 unary (These operation (Only x)) = intro @Checker `hv` Unit
- `yuk____` Apply `hv__` expression `hv` is @(Recursive Expression) x
+ `yuk____` Apply `hv__` expression `hv` x
  `yok____` Check `ha__` Error `ha` Unexpected `la` Ok
  `ha_____` is @Unary `hv` unwrap operation
      `yi_` Negation `hu` (`lu'q` by Double)
@@ -64,7 +62,3 @@ dyadic (These operation (Both (These x y))) = intro @Checker `hv` Unit
      `yi_` Arithmetic `hu` (`lu'q` by Double)
       `la` Comparison `hu` (`lu'q` by Double) `ho'ho'ho` (be `hv'he` Bool)
       `la` Combinated `hu` (`lu'q` by Bool)
-
--- TODO: implement typechecking of this type of expressions
-calling (These name args) = intro @Checker @(AR) `ha` Bool `hv` Unit
- -- `yuk____` Lease `hv__` State `ha` Event `hv` get @Context `yo` find name
